61f8e7f5f36488de71ae05914edf63ccaff837cf
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2024-09-16 16:07:09 -0300

    api keys

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 
-export default function Home() {
+export default function Home() {  
   return (
     <>
       <Header />
